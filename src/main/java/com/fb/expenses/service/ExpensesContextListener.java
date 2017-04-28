0fdf47cdb24d645e3562df52afc3341ac098ef80
f.bertolino <f.bertolino@HP-188.ib-informatica.com>
2017-04-28 17:29:41 +0200

    initialization added

---
+++ src/main/java/com/fb/expenses/service/ExpensesContextListener.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fb.expenses.service;
+
+import com.fb.expenses.entity.ExpenseType;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ *
+ * @author f.bertolino
+ */
+@WebListener
+public class ExpensesContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        ExpenseTypeDAO dao = new ExpenseTypeDAO();
+        dao.deleteAll();
+        
+        List<Pair> data = new ArrayList<>();
+        data.add(new Pair("DOC", "Documenti"));
+        data.add(new Pair("TRASP", "Trasporti"));
+        data.add(new Pair("SAL", "Salute"));
+
+        List<ExpenseType> types = data.stream().map(d -> new ExpenseType((String) d.x, (String) d.y)).collect(Collectors.toList());
+
+        dao.persistAll(types);
+        
+        try {
+            dao.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ExpensesContextListener.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        System.out.println("Shutting down!");
+    }
+    
+    class Pair<X, Y> {
+
+        public final X x;
+        public final Y y;
+
+        public Pair(X x, Y y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
