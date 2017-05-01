818f8f273ccbe7d10ec1147be80d773f77fd1573
eriol1977 <eriol1977@hotmail.com>
2017-05-01 22:33:56 +0200

    minor change

--- src/main/java/com/fb/expenses/service/ExpensesContextListener.java
+++ src/main/java/com/fb/expenses/service/ExpensesContextListener.java
@@ -29,10 +29,15 @@ public class ExpensesContextListener implements ServletContextListener {
         dao.deleteAll();
         
         List<Pair> data = new ArrayList<>();
+        data.add(new Pair("ABB", "Abbigliamento"));
+        data.add(new Pair("ALI", "Alimentari"));
         data.add(new Pair("DOC", "Documenti"));
-        data.add(new Pair("TRASP", "Trasporti"));
+        data.add(new Pair("IGB", "Igiene/Bellezza"));
+        data.add(new Pair("INT", "Intrattenimento"));
+        data.add(new Pair("OGG", "Oggetti"));
         data.add(new Pair("SAL", "Salute"));
-
+        data.add(new Pair("TRA", "Trasporti"));
+        
         List<ExpenseType> types = data.stream().map(d -> new ExpenseType((String) d.x, (String) d.y)).collect(Collectors.toList());
 
         dao.persistAll(types);
